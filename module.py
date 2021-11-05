17029cecf7080f731497e77ad04617ae37a2528b
praveentadepalli <saps.prav9@gmail.com>
2021-11-05 17:46:59 +0530

    Validation changes to module.py

--- module.py
+++ module.py
@@ -38,7 +38,7 @@ def process_input(input_string=None):
         hour,minute = map(int,curr_time.split(":"))
     
     #Validate Hour  and Minutes passed - They cannot be Negative 
-    if hour < 0 or minute < 0:
+    if hour < 0 or minute < 0 or hour > 12 or hour > 60:
         print("Hours/Minutes cannot be Negative")
         return False
     else:
